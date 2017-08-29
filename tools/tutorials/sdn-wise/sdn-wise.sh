f0ebc49a711c0a26ab3dfddd37269e58afec6304
Sebastiano Milardo <s.milardo@hotmail.it>
2017-08-29 16:14:55 +0200

    Update the tutorial to onos-1.10

--- tools/tutorials/sdn-wise/sdn-wise.sh
+++ tools/tutorials/sdn-wise/sdn-wise.sh
@@ -5,14 +5,14 @@ USER="user"
 # Let's free up some space
 rm -Rf ~/contiki-3.0
 rm -Rf ~/contiki
-apt-get remove --purge libreoffice* openjdk-* firefox thunderbird -y
+apt-get remove --purge libreoffice* openjdk-* thunderbird -y
 apt-get clean
 apt-get autoremove -y
 
 # Update to Java 8 and install Ant and Maven
 add-apt-repository ppa:openjdk-r/ppa -y
 apt-get update
-apt-get install openjdk-8-jdk ant maven openvswitch-switch -y
+apt-get install net-tools openjdk-8-jdk ant maven openvswitch-switch -y
 update-java-alternatives -s java-1.8.0-openjdk-i386
 
 # Setup environment variables
@@ -21,11 +21,6 @@ source ~/.bashrc
 
 # Download sdn-wise-contiki
 sudo -u $USER git clone git://github.com/sdnwiselab/sdn-wise-contiki
-
-# Downlad Apache Karaf
-sudo -u $USER mkdir ~/Downloads ~/Applications
-sudo -u $USER wget http://archive.apache.org/dist/karaf/3.0.2/apache-karaf-3.0.2.tar.gz -P ~/Downloads
-sudo -u $USER tar -zxvf ~/Downloads/apache-karaf-3.0.2.tar.gz -C ~/Applications/
 
 # Download ONOS
 sudo -u $USER git clone git://github.com/sdnwiselab/onos
@@ -38,10 +33,6 @@ sudo -u $USER git clone git://github.com/mininet/mininet
 sudo -u $USER echo "export ONOS_ROOT=~/onos" >> ~/.bashrc
 source ~/.bashrc
 source $ONOS_ROOT/tools/dev/bash_profile
-
-# Add ONOS and SDN-WISE to Karaf
-sudo -u $USER sed -i '/featuresRepositories=mvn:org.apache.karaf.features\/standard\/3.0.2\/xml\/features,mvn:org.apache.karaf.features\/enterprise\/3.0.2\/xml\/features,mvn:org.ops4j.pax.web\/pax-web-features\/3.1.2\/xml\/features,mvn:org.apache.karaf.features\/spring\/3.0.2\/xml\/features/c\featuresRepositories=mvn:org.apache.karaf.features\/standard\/3.0.2\/xml\/features,mvn:org.apache.karaf.features\/enterprise\/3.0.2\/xml\/features,mvn:org.ops4j.pax.web\/pax-web-features\/3.1.2\/xml\/features,mvn:org.apache.karaf.features\/spring\/3.0.2\/xml\/features,mvn:org.onosproject\/onos-features\/1.0.2-SNAPSHOT\/xml\/features' /home/user/Applications/apache-karaf-3.0.2/etc/org.apache.karaf.features.cfg
-sudo -u $USER sed -i '/featuresBoot=config,standard,region,package,kar,ssh,management/c\featuresBoot=config,standard,region,package,kar,ssh,management,onos-api,onos-core-trivial,onos-cli,onos-openflow,onos-app-fwd,onos-app-mobility,onos-gui,onos-sdnwise,onos-sdnwise-providers' /home/user/Applications/apache-karaf-3.0.2/etc/org.apache.karaf.features.cfg
 
 # Install Mininet
 mininet/util/install.sh -nfv
