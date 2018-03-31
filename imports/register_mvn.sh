93dd7ce617dbd425e7ccd6f8683319b0cc6e6b5c
Jakob Schaerer <jakob.schaerer@students.unibe.ch>
2018-03-31 15:33:50 +0200

    bugfixes with dependencies

--- imports/register_mvn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mvn install:install-file \
-   -Dfile=sdn-wise-core-4.0.1-SNAPSHOT.jar \
-   -DgroupId=com.github.sdnwiselab \
-   -DartifactId=sdn-wise-core \
-   -Dversion=4.0.1-SNAPSHOT \
-   -Dpackaging=jar \
-   -DgeneratePom=true
