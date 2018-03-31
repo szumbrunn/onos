250ed057485c38a594fcc55460035a7b1a5a972d
Jakob Schaerer <jakob.schaerer@students.unibe.ch>
2018-03-31 15:07:38 +0200

    added libary to mvn and buck

--- dev_run.sh
+++ dev_run.sh
@@ -2,6 +2,9 @@
 
 export ONOS_ROOT=~/onos
 source $ONOS_ROOT/tools/dev/bash_profile
+
+./imports/register_mvn.sh
+
 gnome-terminal -- ./dev_terminal.sh
 cd $ONOS_ROOT && tools/build/onos-buck clean && tools/build/onos-buck build onos --show-output && tools/build/onos-buck run onos-local -- clean debug
 
